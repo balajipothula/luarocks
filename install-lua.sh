1d5a40076efa8ae0952725ad33f570addf55a2bc
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-24 16:40:06 +0530

    Create install-lua.sh

---
+++ install-lua.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 12 August 2019,
+# Description : Installing / Compiling Lua from Source Code.
+
+# update package repositories.
+sudo yum -y update
+
+# installing build essentials.
+sudo yum -y install gcc git make readline-devel unixODBC-devel unzip
+
+# downloading, extracting and compiling lua.
+curl http://www.lua.org/ftp/lua-5.1.1.tar.gz -o $HOME/lua.tar.gz                    && \
+tar -xzf $HOME/lua.tar.gz -C $HOME                                                  && \
+rm  -rf  $HOME/lua.tar.gz                                                           && \
+mv       $HOME/lua-* $HOME/lua                                                      && \
+cd       $HOME/lua                                                                  && \
+sed -i 's/INSTALL_TOP\= \/usr\/local/INSTALL_TOP= ${HOME}\/lua/' $HOME/lua/Makefile && \
+make linux install                                                                  && \
+rm -rf   $HOME/lua/{doc,man,test,COPYRIGHT,HISTORY,INSTALL,README,Makefile}         && \
+echo "export PATH=$PATH:$HOME/lua/bin" >> $HOME/.bashrc
