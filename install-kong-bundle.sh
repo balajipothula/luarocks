e78eac303d27d7e1c50d602dba78212b4584a631
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-30 12:41:33 +0530

    Create install-kong-bundle.sh

---
+++ install-kong-bundle.sh
@@ -0,0 +1,36 @@
+# installing external plugins.
+luarocks install inspect 3.1.1
+luarocks install luasec 0.8
+luarocks install luasocket 3.0-rc1
+luarocks install penlight 1.5.4
+luarocks install lua-resty-http 0.13
+luarocks install lua-resty-jit-uuid 0.0.7
+luarocks install multipart 0.5.5
+luarocks install version 1.0.1
+luarocks install kong-lapis 1.6.0.1
+luarocks install lua-cassandra 1.4.0
+luarocks install pgmoon 1.10.0
+luarocks install luatz 0.4
+luarocks install http 0.3
+luarocks install lua_system_constants 0.1.3
+luarocks install lyaml 6.0-1 # 6.2.3-1 Error: Build error: Failed building.
+luarocks install lua-resty-iputils 0.3.0
+luarocks install luaossl 20181207
+luarocks install luasyslog 1.0.0
+luarocks install lua_pack 1.0.5
+luarocks install lua-resty-dns-client 3.0.2
+luarocks install lua-resty-worker-events 0.3.3
+luarocks install lua-resty-mediador 0.1.2
+luarocks install lua-resty-healthcheck 0.6.2
+luarocks install lua-resty-cookie 0.1.0
+luarocks install lua-resty-mlcache 2.4.0
+
+# installing external kong plugins.
+luarocks install kong-plugin-azure-functions 0.3.0-1
+luarocks install kong-plugin-kubernetes-sidecar-injector 0.2.0-0
+luarocks install kong-plugin-zipkin 0.1.1-0
+luarocks install kong-plugin-serverless-functions 0.2.0-0
+luarocks install kong-prometheus-plugin 0.3.2-2
+luarocks install kong-proxy-cache-plugin 1.2.2-0
+luarocks install kong-plugin-request-transformer 1.2.2-0
+luarocks install kong-plugin-azure-functions 0.3.0-1
