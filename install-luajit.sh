309c9c2065a7a3a48e4efa6dcaf5a956fed77280
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-05 11:54:27 +0530

    Update install-luajit.sh

--- install-luajit.sh
+++ install-luajit.sh
@@ -10,8 +10,7 @@ sudo yum -y update
 # installing build essentials.
 sudo yum -y install gcc make
 
-# downloading, extracting and compiling luajit.
-mkdir -p $HOME/luajit                                                      && \
+# luajit downloading, extracting and compiling.
 curl http://luajit.org/download/LuaJIT-2.0.5.tar.gz -o $HOME/LuaJIT.tar.gz && \
 tar -xzf $HOME/LuaJIT.tar.gz -C $HOME                                      && \
 rm  -rf  $HOME/LuaJIT.tar.gz                                               && \
