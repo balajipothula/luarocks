eab58be6080288462278f10442d4e6200b849791
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-05 14:15:07 +0530

    Update install-lua.sh

--- install-lua.sh
+++ install-lua.sh
@@ -8,15 +8,15 @@
 sudo yum -y update
 
 # installing build essentials.
-sudo yum -y install gcc git libyaml make openssl-devel readline-devel unixODBC-devel unzip
+sudo yum -y install gcc make readline-devel
 
 # lua downloading, extracting and compiling.
 curl -JL http://www.lua.org/ftp/lua-5.1.1.tar.gz -o $HOME/lua.tar.gz                && \
 tar -xzf $HOME/lua.tar.gz -C $HOME                                                  && \
 rm  -rf  $HOME/lua.tar.gz                                                           && \
 mv       $HOME/lua-* $HOME/Lua                                                      && \
-cd       $HOME/Lua                                                                  && \
 sed -i 's/INSTALL_TOP= \/usr\/local/INSTALL_TOP= ${HOME}\/lua/' $HOME/Lua/Makefile  && \
+cd       $HOME/Lua                                                                  && \
 make linux install                                                                  && \
 cd       $HOME                                                                      && \
 rm -rf   $HOME/Lua                                                                  && \
