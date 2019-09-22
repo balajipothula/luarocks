5af1000f2aaebccea22caaee0a440e90ddba2167
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-22 17:06:20 +0530

    Update install-luajit.sh

--- install-luajit.sh
+++ install-luajit.sh
@@ -11,14 +11,14 @@ sudo yum -y update
 sudo yum -y install gcc make
 
 # luajit downloading, extracting and compiling.
-curl http://luajit.org/download/LuaJIT-2.0.5.tar.gz -o $HOME/LuaJIT.tar.gz && \
-tar -xzf $HOME/LuaJIT.tar.gz -C $HOME                                      && \
-rm  -rf  $HOME/LuaJIT.tar.gz                                               && \
-mv       $HOME/LuaJIT-* $HOME/LuaJIT                                       && \
-cd       $HOME/LuaJIT                                                      && \
-make                                                                       && \
-make install CC="gcc -m32" TARGET_SYS=Linux PREFIX=$HOME/luajit            && \
-cd       $HOME                                                             && \
-rm  -rf  $HOME/LuaJIT                                                      && \
-echo "export PATH=$PATH:$HOME/luajit/bin" >> $HOME/.bashrc                 && \
+curl http://luajit.org/download/LuaJIT-2.0.5.tar.gz -o $HOME/luajitsrc.tar.gz && \
+tar -xzf $HOME/luajitsrc.tar.gz -C $HOME                                      && \
+rm  -rf  $HOME/luajitsrc.tar.gz                                               && \
+mv       $HOME/LuaJIT-* $HOME/luajitsrc                                       && \
+cd       $HOME/luajitsrc                                                      && \
+make                                                                          && \
+make install CC="gcc -m32" TARGET_SYS=Linux PREFIX=$HOME/luajit               && \
+cd       $HOME                                                                && \
+rm  -rf  $HOME/luajitsrc                                                      && \
+echo "export PATH=$PATH:$HOME/luajit/bin" >> $HOME/.bashrc                    && \
 exec $BASH
