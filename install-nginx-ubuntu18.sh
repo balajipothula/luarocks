e95d2b7f6db73c6be758fcbf1bc4f537c79e5681
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-23 16:18:03 +0530

    Update install-nginx-ubuntu18.sh

--- install-nginx-ubuntu18.sh
+++ install-nginx-ubuntu18.sh
@@ -41,6 +41,7 @@ cd        $HOME/nginxsrc                                                       &
             --http-log-path=$HOME/nginx/log/access.log                            \
             --pid-path=$HOME/nginx/log/nginx.pid                                  \
             --lock-path=$HOME/nginx/log/nginx.lock                                \
+            --with-cpu-opt='generic'                                              \
             --with-cc-opt='-static -static-libgcc'                                \
             --with-ld-opt='-lrt -static'                                          \
             --with-pcre=$HOME/pcresrc                                             \
