837ab6a3523caef419a9ab9a028d3a03afa9510a
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-23 10:19:30 +0530

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -7,59 +7,42 @@
 # update package repositories.
 sudo yum -y update
 
-# installing build essentials.
-sudo yum -y install gcc gcc-c++ make autoconf automake
+# build essentials installing.
+sudo yum -y install gcc gcc-c++ glibc-devel make autoconf automake
 
-# zlib downloading, extracting and compiling.
+# zlib downloading and extracting.
 curl -JL https://zlib.net/zlib-1.2.11.tar.gz -o $HOME/zlib.tar.gz && \
 tar -xzf $HOME/zlib.tar.gz -C $HOME                               && \
 rm  -rf  $HOME/zlib.tar.gz                                        && \
-mv       $HOME/zlib-* $HOME/zlibsrc                               && \
-cd       $HOME/zlibsrc                                            && \
-./configure --prefix=$HOME/zlib                                   && \
-make install                                                      && \
-cd       $HOME                                                    && \
-rm  -rf  $HOME/zlibsrc
+mv       $HOME/zlib-* $HOME/zlibsrc
 
-# pcre downloading, extracting and compiling.
+# pcre downloading and extracting.
 curl -JL https://ftp.pcre.org/pub/pcre/pcre-8.41.tar.gz -o $HOME/pcre.tar.gz && \
 tar -xzf $HOME/pcre.tar.gz -C $HOME                                          && \
 rm  -rf  $HOME/pcre.tar.gz                                                   && \
-mv       $HOME/pcre-* $HOME/pcresrc                                          && \
-cd       $HOME/pcresrc                                                       && \
-./configure --prefix=$HOME/pcre                                              && \
-make install                                                                 && \
-cd       $HOME                                                               && \
-rm  -rf  $HOME/pcresrc
+mv       $HOME/pcre-* $HOME/pcresrc
 
-# libressl downloading, extracting and compiling.
+# libressl downloading and extracting.
 curl -JL https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.6.1.tar.gz -o $HOME/libressl.tar.gz && \
 tar -xzf $HOME/libressl.tar.gz -C $HOME                                                              && \
 rm  -rf  $HOME/libressl.tar.gz                                                                       && \
-mv       $HOME/libressl-* $HOME/libresslsrc                                                          && \
-cd       $HOME/libresslsrc                                                                           && \
-./configure --prefix=$HOME/libressl                                                                  && \
-make install                                                                                         && \
-cd       $HOME                                                                                       && \
-rm  -rf  $HOME/libresslsrc
+mv       $HOME/libressl-* $HOME/libresslsrc
 
 # nginx downloading, extracting and compiling.
-curl -JL http://nginx.org/download/nginx-1.12.1.tar.gz -o $HOME/nginx.tar.gz && \
-tar -xzf $HOME/nginx.tar.gz -C $HOME                                         && \
-rm  -rf  $HOME/nginx.tar.gz                                                  && \
-mv       $HOME/nginx-* $HOME/nginxsrc                                        && \
-cd       $HOME/nginxsrc                                                      && \
-./configure --prefix=$HOME/nginx                                                \
-            --without-http_scgi_module                                          \
-            --without-http_uwsgi_module                                         \
-            --without-http_fastcgi_module                                       \
-            --with-cc-opt='-static -static-libgcc'                              \
-            --with-cpu-opt=generic                                              \
-            --with-pcre=$HOME/pcre                                              \
-            --with-pcre-jit                                                     \
-            --with-zlib=$HOME/zlib                                              \
-            --with-openssl=$HOME/libressl                                       \
-            --with-http_ssl_module                                           && \
-make install                                                                 && \
-cd       $HOME                                                               && \
-rm  -rf  $HOME/nginxsrc
+curl -JL  https://nginx.org/download/nginx-1.17.3.tar.gz -o $HOME/nginx.tar.gz && \
+tar  -xzf $HOME/nginx.tar.gz -C $HOME                                          && \
+rm   -rf  $HOME/nginx.tar.gz                                                   && \
+mv        $HOME/nginx-* $HOME/nginxsrc                                         && \
+cd        $HOME/nginxsrc                                                       && \
+./configure --prefix=$HOME/nginx                                                  \
+            --sbin-path=$HOME/nginx/bin/nginx                                     \
+            --conf-path=$HOME/nginx/conf/nginx.conf                               \
+            --error-log-path=$HOME/nginx/log/error.log                            \
+            --http-log-path=$HOME/nginx/log/access.log                            \
+            --pid-path=$HOME/nginx/log/nginx.pid                                  \
+            --lock-path=$HOME/nginx/log/nginx.lock                                \
+            --with-pcre=$HOME/pcresrc                                             \
+            --with-pcre-jit                                                       \
+            --with-zlib=$HOME/zlibsrc                                             \
+            --with-openssl=$HOME/libresslsrc                                      \
+            --with-http_ssl_module
